a01bcb0b43a7ed284dcd19ef135221d09b03bc90
Beth Johnson <bethjohnson3@gmail.com>
2017-11-15 16:29:52 -0800

    (init)

---
+++ server/bodyParserJSON.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client')));
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} to ${req.url} with headers ${req.headers['content-type']}`);
+  next();
+})
+
+app.get('*', (req, res) => {
+  console.log('req.body', req.body);
+  console.log('req.query', req.query);
+  res.end();
+});
+
+app.post('*', (req, res) => {
+  console.log('req.body', req.body);
+  console.log('req.query', req.query);
+  res.end();
+});
+
+app.listen(3000);
---
+++ server/reqOn.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client')));
+
+app.use((req, res, next) => {
+  console.log(`${req.method} to ${req.url} with headers ${req.headers['content-type']}`);
+  next();
+});
+
+app.use((req, res, next) => {
+  let body = [];
+  req.on('data', function(chunk) {
+    body.push(chunk)
+  }).on('end', function() {
+    req.body = Buffer.concat(body).toString()
+    next();
+  });
+})
+
+app.get('*', (req, res) => {
+  console.log('req.body', req.body);
+  console.log('req.query', req.query);
+  res.end();
+});
+
+app.post('*', (req, res) => {
+  console.log('req.body', req.body);
+  console.log('req.query', req.query);
+  res.end();
+});
+
+app.listen(3000);
